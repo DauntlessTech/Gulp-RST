6567cf35e67546be1cdf80232a7bf9e3e48cd990
Zachary Foutz <zacharyfoutz@gmail.com>
2016-06-11 19:31:08 -0700

    feat: Added home icon, changed ui colors 	Navbar home button was added, and button colors were changed from dark to light.

--- src/assets/js/src/components/Navbar.jsx
+++ src/assets/js/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ var NavBar = React.createClass({
   render: function(){
     return(
       <nav className="row navbar">
-        <a className="brand u-pull-left" href="#!">Gulp-RST</a>
+        <a className="brand u-pull-left" href="#!"><i className="material-icons">home</i></a>
         <Button link_class="button button-primary u-pull-right" text="docs" href="https://www.google.com"/>
       </nav>
     );
